8cfd3b9e0c016228eebc48fe9c67d90b5ffccc46
David Burns <davidburns@iinet.net.au>
2018-02-05 19:54:06 +1000

    Swaps inputs and outputs around so that they better match the order of the pixel reference order x,y

--- mandelgo.go
+++ mandelgo.go
@@ -15,10 +15,10 @@ func main() {
     // work out conversion factors for going from the canvas position to the actual imaginary and 
     // real axis values you want
     // cIm is imaginary, cRe is real
-    cImMin := -2.0
-    cImMax := 2.0
     cReMin := -3.0
     cReMax := 1.0
+    cImMin := -2.0
+    cImMax := 2.0
     reCanvasConversion := (cReMax - cReMin)/float64(size)
     imCanvasConversion := (cImMax - cImMin)/float64(size)
 
@@ -30,7 +30,7 @@ func main() {
 	    // also need to be careful about how i and j grow across the canvas
 	    cRe := cReMin + float64(i)*reCanvasConversion
 	    cIm := cImMax - float64(j)*imCanvasConversion
-	    colour := escapeTest(cIm, cRe, 20, 0.1)
+	    colour := escapeTest(cRe, cIm, 20, 0.1)
 	    canvas.SetRGB(colour, colour, colour)
 	    canvas.SetPixel(i, j)
 	}
@@ -40,26 +40,26 @@ func main() {
 }
 
 // zIm,a is the imaginary component, zRe,cRe is the real
-func zCubed(zIm, zRe, cIm, cRe float64) (float64, float64) {
+func zCubed(zRe, zIm, cRe, cIm float64) (float64, float64) {
     zReNext := cRe - 3*math.Pow(zIm, 2)*zRe + math.Pow(zRe, 3)
     zImNext := cIm - math.Pow(zIm, 3) + 3*zIm*math.Pow(zRe, 2)
-    return zImNext, zReNext
+    return zReNext, zImNext 
 }
 
 // zIm,cIm is the imaginary component, zRe,cRe is the real
-func zSquared(zIm, zRe, cIm, cRe float64) (float64, float64) {
+func zSquared(zRe, zIm, cRe, cIm float64) (float64, float64) {
     zReNext := cRe - math.Pow(zIm, 2) + math.Pow(zRe, 2)
     zImNext := cIm + 2*zIm*zRe 
-    return zImNext, zReNext
+    return zReNext, zImNext 
 }
 
-func escapeTest(cIm, cRe float64, maxLoop int, threshhold float64) float64 {
-    zIm := 0.0
+func escapeTest(cRe, cIm float64, maxLoop int, threshhold float64) float64 {
     zRe := 0.0
+    zIm := 0.0
     for i := 0; i < maxLoop; i++ {
-	zIm, zRe = zSquared(zIm, zRe, cIm, cRe)
+	zRe, zIm = zSquared(zRe, zIm, cRe, cIm)
     }
-    if (math.Hypot(zIm, zRe)) < threshhold {
+    if (math.Hypot(zRe, zIm)) < threshhold {
 	return 0.0
     }
     return 1.0

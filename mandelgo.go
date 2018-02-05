36d6df6693b715dfef06d589fab30a6db209bb3c
David Burns <davidburns@iinet.net.au>
2018-02-05 19:26:37 +1000

    Changes real an imaginary variable names

--- mandelgo.go
+++ mandelgo.go
@@ -6,7 +6,7 @@ import "github.com/fogleman/gg"
 
 func main() {
     //fmt.Println(complexFunction(2,3))
-    var size int = 1000
+    size := 1000
     canvas := gg.NewContext(size,size)
     //canvas.DrawCircle(500,500,400)
     //canvas.SetRGB(0,0,0)
@@ -14,20 +14,23 @@ func main() {
 
     // work out conversion factors for going from the canvas position to the actual imaginary and 
     // real axis values you want
-    a_min := -3.0
-    a_max := 1.0
-    b_min := -2.0
-    b_max := 2.0
-    iCanvasConversion := (a_max - a_min)/float64(size)
-    jCanvasConversion := (b_max - b_min)/float64(size)
+    // cIm is imaginary, cRe is real
+    cImMin := -2.0
+    cImMax := 2.0
+    cReMin := -3.0
+    cReMax := 1.0
+    reCanvasConversion := (cReMax - cReMin)/float64(size)
+    imCanvasConversion := (cImMax - cImMin)/float64(size)
 
+    // i dont know if i is the horizontal or vertical axis
     // Loop the canvas
     for i := 0; i < size; i++ {
 	for j := 0; j < size; j++ {
 	    // find the complex representation of the canvas position
-	    a := a_min + float64(i)*iCanvasConversion
-	    b := b_max - float64(j)*jCanvasConversion // this is because y decreases downwards but x increases across
-	    colour := escapeTest(a,b,10,0.001)
+	    // also need to be careful about how i and j grow across the canvas
+	    cRe := cReMin + float64(i)*reCanvasConversion
+	    cIm := cImMax - float64(j)*imCanvasConversion
+	    colour := escapeTest(cIm,cRe,20,0.1)
 	    canvas.SetRGB(colour,colour,colour)
 	    canvas.SetPixel(i,j)
 	}
@@ -36,28 +39,28 @@ func main() {
     canvas.SavePNG("out/out.png")
 }
 
-// x,a is the imaginary component, y,b is the real
-func zCubed(x, y, a, b float64) (float64, float64) {
-    var yn float64 = b - 3*x*x*y + y*y*y
-    var xn float64 = a - x*x*x + 3*x*y*y
-    return xn, yn
+// zIm,a is the imaginary component, zRe,cRe is the real
+func zCubed(zIm, zRe, cIm, cRe float64) (float64, float64) {
+    zReNext := cRe - 3*zIm*zIm*zRe + zRe*zRe*zRe
+    zImNext := cIm - zIm*zIm*zIm + 3*zIm*zRe*zRe
+    return zImNext, zReNext
 }
 
-// x,a is the imaginary component, y,b is the real
-func zSquared(x, y, a, b float64) (float64, float64) {
-    var yn float64 = b - x*x + y*y
-    var xn float64 = a + 2*x*y 
-    return xn, yn
+// zIm,cIm is the imaginary component, zRe,cRe is the real
+func zSquared(zIm, zRe, cIm, cRe float64) (float64, float64) {
+    zReNext := cRe - zIm*zIm + zRe*zRe
+    zImNext := cIm + 2*zIm*zRe 
+    return zImNext, zReNext
 }
 
-func escapeTest(a, b float64, maxLoop int, threshhold float64) float64 {
-    var x float64 = 0
-    var y float64 = 0
+func escapeTest(cIm, cRe float64, maxLoop int, threshhold float64) float64 {
+    zIm := 0.0
+    zRe := 0.0
     for i := 0; i < maxLoop; i++ {
-	x, y = zSquared(x, y, a, b)
+	zIm, zRe = zSquared(zIm, zRe, cIm, cRe)
     }
-    if (x*x + y*y) < threshhold {
-	return 0
+    if (zIm*zIm + zRe*zRe) < threshhold {
+	return 0.0
     }
-    return 1
+    return 1.0
 }

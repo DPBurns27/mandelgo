ac4bf03374044a8f6075cc7296ccc2a474ef0a55
David Burns <davidburns@iinet.net.au>
2018-02-06 21:30:17 +1000

    Adds zooming

--- mandelgo.go
+++ mandelgo.go
@@ -6,37 +6,43 @@ import "github.com/fogleman/gg"
 
 func main() {
     fmt.Println("Running...")
-    size := 4000
+    frames := 10
+    zoomFactor := 0.5
+    zoomRe := 0.25
+    zoomIm := 0.0
+    size := 500
     canvas := gg.NewContext(size, size)
     //canvas.DrawCircle(500,500,400)
     //canvas.SetRGB(0,0,0)
     //canvas.Fill()
 
-    // work out conversion factors for going from the canvas position to the actual imaginary and 
-    // real axis values you want
-    // cIm is imaginary, cRe is real
-    cReMin := -3.0
-    cReMax := 1.0
-    cImMin := -2.0
-    cImMax := 2.0
-    reCanvasConversion := (cReMax - cReMin)/float64(size)
-    imCanvasConversion := (cImMax - cImMin)/float64(size)
-
-    // i dont know if i is the horizontal or vertical axis
-    // Loop the canvas
-    for i := 0; i < size; i++ {
-	cRe := cReMin + float64(i)*reCanvasConversion
-	for j := 0; j < size; j++ {
-	    // find the complex representation of the canvas position
-	    // also need to be careful about how i and j grow across the canvas
-	    cIm := cImMax - float64(j)*imCanvasConversion
-	    colour := escapeTest(cRe, cIm, 100, 4)
-	    canvas.SetRGB(colour, colour, colour)
-	    canvas.SetPixel(i, j)
-	}
+    for k := 0; k < frames; k++ {
+        // work out conversion factors for going from the canvas position to the actual imaginary and 
+        // real axis values you want
+        // cIm is imaginary, cRe is real
+        cReMin := -3.0*math.Pow(zoomFactor,float64(k)) 
+        cReMax := 1.0*math.Pow(zoomFactor,float64(k))
+        cImMin := -2.0*math.Pow(zoomFactor,float64(k)) 
+        cImMax := 2.0*math.Pow(zoomFactor,float64(k)) 
+        reCanvasConversion := (cReMax - cReMin)/float64(size)
+        imCanvasConversion := (cImMax - cImMin)/float64(size)
+    
+        // i dont know if i is the horizontal or vertical axis
+        // Loop the canvas
+        for i := 0; i < size; i++ {
+    	cRe := cReMin + float64(i)*reCanvasConversion
+    	for j := 0; j < size; j++ {
+    	    // find the complex representation of the canvas position
+    	    // also need to be careful about how i and j grow across the canvas
+    	    cIm := cImMax - float64(j)*imCanvasConversion
+    	    colour := escapeTest(cRe, cIm, 100, 4)
+    	    canvas.SetRGB(colour, colour, colour)
+    	    canvas.SetPixel(i, j)
+    	}
+        }
+	filename := fmt.Sprintf("out/out%d.png", k) 
+        canvas.SavePNG(filename)
     }
-
-    canvas.SavePNG("out/out.png")
 }
 
 // zIm,a is the imaginary component, zRe,cRe is the real
